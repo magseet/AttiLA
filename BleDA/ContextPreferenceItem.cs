30931b2d00bf471d5729949ae9033c842b8caaa4
riccardo <riccardo.cantoro@gmail.com>
2013-10-26 03:20:42 +0200

    Sinchronous problems in page solved. Localization update works.

---
+++ BleDA/ContextPreferenceItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BleDA.LocalizationService;
+using AttiLA.Data.Entities;
+using MongoDB.Bson;
+
+namespace BleDA
+{
+    public class ContextPreferenceItem
+    {
+        public String ContextName { get; set; }
+        public String Preference { get; set; }
+    }
+}
